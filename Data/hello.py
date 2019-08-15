222b54f3180f5f9de8401515dca40a5010be321f
Nadezhda <0607707@gmail.com>
2019-08-15 11:43:29 +0300

    Modify

---
+++ Data/hello.py
@@ -0,0 +1,12 @@
+# Start
+# Programm for hello
+from time import sleep
+
+for i in range(10):
+    print(10 - i)
+    sleep((1 - i/10)/3)
+
+print("Hello world")
+print("Modify")
+print("Bye world!")
+# End
